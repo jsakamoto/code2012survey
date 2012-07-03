f698bbe3ec2f62ad087fd6b86c6f518d06c3ef3a
jsakamoto <junichi.jp@gmail.com>
2012-07-03 19:29:47 +0900

    Code2012Survey - 1st commit.

---
+++ Code2012Survey/Controllers/SurveyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Code2012Survey.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: /Survey/
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Code2012Survey/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Code2012Survey.MvcApplication" Language="C#" %>
