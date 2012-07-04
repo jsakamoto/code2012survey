e3aacdc25e86e8a5de9a01127b2a8c3b10d79dc8
jsakamoto <junichi.jp@gmail.com>
2012-07-04 22:46:43 +0900

    A 1st step of localization.

--- Code2012Survey/Global.asax.cs
+++ Code2012Survey/Global.asax.cs
@@ -1,14 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+//using Code2012Survey.LangPack;
 
 namespace Code2012Survey
 {
@@ -24,6 +24,9 @@ namespace Code2012Survey
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            //Resource1.Culture = CultureInfo.GetCultureInfo("ja-jp");
+            //Localize.Culture = CultureInfo.GetCultureInfo("ja-jp");
         }
     }
 }
